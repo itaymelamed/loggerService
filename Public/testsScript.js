dd8a1aa193406e0b3dd131775ed287b9a4675f98
itaymelamed <itaymelamed@ymail.com>
2017-12-09 10:40:59 +0200

    test

--- Public/testsScript.js
+++ Public/testsScript.js
@@ -21,11 +21,11 @@ function ShowTest(testNumber, testRunId, testName){
     var steps = testSteps[0].map((s,i) => {
 
         return (
-          <div key={i} className="step">{i+1+". " + s}<span id="vi">   ✔</span></div>
+          <div key={i} className="step testdetails-detail">{i+1+". " + s}<span id="vi">   ✔</span></div>
         )
     })
     if (testStat)
-      steps.push(<div>{steps.length+1 + ". " +lastStep}<span id="x">    ✘</span></div>);
+      steps.push(<div className="step testdetails-detail">{steps.length+1 + ". " +lastStep}<span id="x">    ✘</span></div>);
 
     var testResultsComp = testResults.map(d => {
       if (d.Status == "Passed"){
@@ -34,7 +34,7 @@ function ShowTest(testNumber, testRunId, testName){
             <div id="testdetails-title"><span id="titlehelper">Test Details</span></div>
             <div className="testdetails-detail">Url: {d.Url}</div>
             <div className="testdetails-detail">Steps: <div>{steps}</div></div>    
-            <div id="scrennshot"><img src={d.ScreenShot} id="photo" width="100%" height="40%"/></div>
+            <div id="scrennshot"><a href={d.ScreenShot} target="_blank"><img src={d.ScreenShot} id="photo" width="100%" height="40%"/></a></div>
           </div>
         )
       }
@@ -53,7 +53,7 @@ function ShowTest(testNumber, testRunId, testName){
             <div className="testdetails-detail">Error Message: {d.ErrorMessage}</div>
             <div className="testdetails-detail">Url: {d.Url}</div>
             <div className="testdetails-detail">Steps: <div>{steps}</div></div>    
-            <div id="scrennshot"><img src={d.ScreenShot} id="photo" width="100%" height="40%"/></div>
+            <div id="scrennshot"><a href={d.ScreenShot} target="_blank"><img src={d.ScreenShot} id="photo" width="100%" height="40%"/></a></div>
           </div>
         )
       }
@@ -71,7 +71,7 @@ function RenderResults(id, siteName){
           clearInterval(intervaliD);
           console.log("stop");
           ShowTest(result.TestNumber, id, result.TestName);
-          intervaliD = setInterval(() => ShowTest(result.TestNumber, id, result.TestName), 3000);
+          intervaliD = setInterval(() => ShowTest(result.TestNumber, id, result.TestName), 1000);
         }}>{result.TestName}</div>
       )
     });
@@ -148,6 +148,14 @@ function CalculateSucRate(results, status){
   return sucRate;
 }
 
+function DurationController(testResults)
+{
+  if(testResults.Results.SentToHub == 0 && testResults.Results.Running == 0)
+  {
+    return testResults.Duration.slice(3, 8);
+  }
+}
+
 function RenderTestRuns(){
   GetTestsRuns().then(data => {
     var idCompo = data.reverse().slice(0, 5).map(test => {
@@ -155,8 +163,8 @@ function RenderTestRuns(){
         <div id="testrun1" className="testrunsids">
           <div className="title">{test.SiteName}</div>
           <div className="env details">Env: {test.Env}</div>
-          <div className="date details">Date: {test.Date}</div>
-          <div className="durations details">Duration: {test.Duration} Minutes</div>
+          <div className="date details">Date: {test.Date.slice(3,14)}</div>
+          <div className="durations details">Duration: {DurationController(test)}</div>
           <div className="details">Status: {GetTestRunStatus(test.Results)}</div>          
           <div className="passed details">Passed: {test.Results.Passed} ({CalculateSucRate(test.Results, test.Results.Passed)}%)</div>
           <div className="failed details">Failed: {test.Results.Failed} ({CalculateSucRate(test.Results, test.Results.Failed)}%)</div>
