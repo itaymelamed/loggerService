4c6b7f3aedc7a0eccd0654762b51a6271a145fb3
Itay Melamed <itaymelamed@minutes-MacBook-Pro.local>
2018-02-22 16:49:01 +0200

    test

--- Routes/api.js
+++ Routes/api.js
@@ -21,9 +21,8 @@ router.get('/tests', function(req, res, next){
 
 router.get('/adsTxt', function(req, res, next){
     const RunModel = mongoose.model('Run', RunsSchema, `Runs`);
-    RunModel.find({}).then(function(runs){
-        var tests = runs.filter(run => run.Category == "AdsTxt");
-        res.send(tests.map(test => test));
+    RunModel.find({Cartegory : "AdsTxt"}).then(function(runs){
+        res.send(runs.map(run => run));
     });
 });
 
